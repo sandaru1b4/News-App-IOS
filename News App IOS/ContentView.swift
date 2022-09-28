61dbd3ed42a75bf5e264216197ca59cf692c4f52
Achitha Sandaruwan <sandaruwanb4@gmail.com>
2022-09-28 05:56:03 +0530

    initial commit

---
+++ News App IOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  News App IOS
+//
+//  Created by Achitha Sandaruwan on 2022-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News App IOS/News_App_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_App_IOSApp.swift
+//  News App IOS
+//
+//  Created by Achitha Sandaruwan on 2022-09-28.
+//
+
+import SwiftUI
+
+@main
+struct News_App_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
